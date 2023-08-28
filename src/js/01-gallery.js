496b1f00f5f0808e73524020c16d55dc38aae7a9
Gugiet <mguzowski92@gmail.com>
2023-08-28 16:47:11 +0200

    c

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,57 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createGallery = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+        </a>
+      </div>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', createGallery);
+
+gallery.addEventListener('click', selectItem);
+
+function selectItem(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: instance => {
+        document.removeEventListener('keydown', onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
