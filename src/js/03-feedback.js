9bc763a0cdca3f87f47b067bc2cb6c1c73e8b2c0
Gugiet <mguzowski92@gmail.com>
2023-08-29 19:42:13 +0200

    Update 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+const throttle = require('lodash.throttle');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const submitBtn = document.querySelector('button');
 
+let feedbackForm = {
+  email: '',
+  message: '',
+};
+
+email.addEventListener('input', event => {
+  const email = event.target.value;
+
+  const savedSettings = localStorage.getItem('feedback-form-state');
+  const parsedSettings = JSON.parse(savedSettings);
+
+  const settings = parsedSettings ? parsedSettings : feedbackForm;
+
+  feedbackForm = { ...settings, email: email };
+  throttledSaveStateToLocalStorage();
+});
+
+message.addEventListener('input', event => {
+  const message = event.target.value;
+
+  const savedSettings = localStorage.getItem('feedback-form-state');
+  const parsedSettings = JSON.parse(savedSettings);
+
+  const settings = parsedSettings ? parsedSettings : feedbackForm;
+
+  feedbackForm = { ...settings, message: message };
+  throttledSaveStateToLocalStorage();
+});
+
+function saveStateToLocalStorage() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm));
+}
+
+const throttledSaveStateToLocalStorage = throttle(saveStateToLocalStorage, 500);
+
+submitBtn.addEventListener('click', event => {
+  event.preventDefault();
+
+  const savedSettings = localStorage.getItem('feedback-form-state');
+  const parsedSettings = JSON.parse(savedSettings);
+
+  console.log(parsedSettings);
+  email.value = '';
+  message.value = '';
+});
+
+const savedSettings = localStorage.getItem('feedback-form-state');
+const parsedSettings = JSON.parse(savedSettings);
+
+email.value = parsedSettings.email;
+message.value = parsedSettings.message;
